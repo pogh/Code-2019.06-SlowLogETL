e92b8ac1449929d123cfbf4a7f308bb58bb252b6
pogh <github@spellman.info>
2019-06-10 13:04:53 +0200

    Initial Check-in

---
+++ Load_MariaDbSlowLog.sql
@@ -0,0 +1,169 @@
+﻿
+CREATE PROCEDURE [staging].[Load_MariaDbSlowLog]
+AS
+BEGIN 
+
+	SET NOCOUNT ON;
+
+	---------------------------------------------------------------------------
+
+	-- Use these to delimit block of lines that belong together
+	DECLARE @n INT;
+	DECLARE @m INT;
+	DECLARE @maxLineNumber INT;
+	
+	-- Used to hold concat'ed lines
+	DECLARE @concatLineText NVARCHAR(MAX);
+
+	-- Keep count of how many entries we have
+	DECLARE @entryNumber int = 0;
+
+	---------------------------------------------------------------------------
+	-- Initialise variables
+	
+	SELECT @n = MIN(LineNumber)
+	FROM [staging].[TextFile]
+	WHERE LineText LIKE '# %';
+
+	SELECT @maxLineNumber = MAX(LineNumber)
+	FROM [staging].[TextFile];
+
+	---------------------------------------------------------------------------
+
+	WHILE(@n < @maxLineNumber)
+	BEGIN
+
+		SET @entryNumber += 1;
+
+		-- Find the end of the current block
+		SELECT @m = MIN(LineNumber)
+		FROM [staging].[TextFile]
+		WHERE LineText NOT LIKE '# %'
+		  AND LineNumber > @n;
+
+		-- Concat the lines together
+		-- Replace double empty space '  ' with # to make split easier
+		-- User@host sometimes has pointless double space so replace with single space
+		SET @concatLineText = NULL;
+		SELECT @concatLineText = 
+			COALESCE(@concatLineText, '') + 
+			CASE WHEN LineText LIKE '# User@Host: %' THEN REPLACE(LineText, '  ', ' ') 
+				 ELSE REPLACE(LineText, '  ', '#') END
+		FROM [staging].[TextFile]
+		WHERE LineNumber >= @n
+		  AND LineNumber < @m
+		ORDER BY LineNumber;
+
+		-- See what's happening
+		--PRINT @concatLineText;
+
+		-- Used to step through the concat'ed string
+		DECLARE @i INT = 0;
+		DECLARE @j INT = 0;
+		DECLARE @length INT = LEN(@concatLineText);
+
+		DECLARE @ValueKeyPair VARCHAR(MAX);
+		DECLARE @charIndex INT;
+
+		-- Time values only appears now and then so remember it
+		DECLARE @time NVARCHAR(16);
+
+		-- Easiest to store extracted values as table
+		DECLARE @values AS TABLE(
+			ValueKey NVARCHAR(32),
+			Value NVARCHAR(128)
+			)
+
+		WHILE(@j < @length)
+		BEGIN
+			SET @j = CHARINDEX('#', @concatLineText, @i);
+
+			IF(@j = 0)
+				SET @j = @length + 1;
+
+			-- Extract value/key pair
+			SET @ValueKeyPair = LTRIM(RTRIM(SUBSTRING(@concatLineText, @i, @j - @i)));
+			SET @charIndex = CHARINDEX(': ', @ValueKeyPair);
+
+			IF(@charIndex = 0) -- the time part of the date/time sits on it's own line
+			BEGIN
+				SELECT @time = Value + ' ' + @ValueKeyPair
+				FROM @values
+				WHERE ValueKey = 'Time';
+
+				UPDATE @values
+				SET Value = @time
+				WHERE ValueKey = 'Time';
+			END
+			ELSE
+			BEGIN
+				INSERT INTO @values(ValueKey, Value)
+				VALUES(SUBSTRING(@ValueKeyPair, 1, @charIndex - 1), SUBSTRING(@ValueKeyPair, @charIndex + 2, LEN(@ValueKeyPair) - @charIndex + 1));
+			END
+
+			SET @i = @j + 1;
+
+		END
+
+		----------------------------------------------------------------------------------------
+		-- Move to the next block and concat the lines together to get the statement
+		SET @n = @m;
+
+		SELECT @m = MIN(LineNumber)
+		FROM staging.TextFile
+		WHERE LineText LIKE '# %'
+		  AND LineNumber > @n;
+
+		IF(@m IS NULL)
+		BEGIN
+			SELECT @m = MAX(LineNumber) + 1
+			FROM staging.TextFile;
+		END
+
+		SET @concatLineText = NULL;
+		SELECT @concatLineText = COALESCE(@concatLineText, '') + LineText + CHAR(13)
+		FROM staging.TextFile
+		WHERE LineNumber >= @n 
+			AND LineNumber < @m
+		ORDER BY LineNumber;
+
+		----------------------------------------------------------------------------------------
+
+		INSERT INTO dbo.MariaDbSlowLog (QueryDateTime, UserAndHost, ThreadId, SchemaName, QueryCacheHit, QueryTimeSeconds, LockTimeSeconds, RowsSent, RowsExamined, RowsAffected, FullScan, FullJoin, TmpTable, TmpTableOnDisk, Filesort, FilesortOnDisk, MergePasses, PriorityQueue, QueryStatement)
+		SELECT 
+			CONVERT(DATETIME2(0), ISNULL(Q.[Time], @time), 12) QueryDateTime, 
+			Q.[User@Host] UserAndHost, 
+			Q.[Thread_id] ThreadId, 
+			Q.[Schema] SchemaName, 
+			CASE Q.[QC_hit] WHEN 'No' THEN 0 WHEN 'Yes' THEN 1 END QueryCacheHit, 
+			Q.[Query_time] QueryTimeSeconds, 
+			Q.[Lock_time] LockTimeSeconds, 
+			Q.[Rows_sent] RowsSent, 
+			Q.[Rows_examined] RowsExamined, 
+			Q.[Rows_affected] RowsAffected, 
+			CASE Q.[Full_scan] WHEN 'No' THEN 0 WHEN 'Yes' THEN 1 END FullScan, 
+			CASE Q.[Full_join] WHEN 'No' THEN 0 WHEN 'Yes' THEN 1 END FullJoin, 
+			CASE Q.[Tmp_table] WHEN 'No' THEN 0 WHEN 'Yes' THEN 1 END TmpTable, 
+			CASE Q.[Tmp_table_on_disk] WHEN 'No' THEN 0 WHEN 'Yes' THEN 1 END TmpTableOnDisk, 
+			CASE Q.[Filesort] WHEN 'No' THEN 0 WHEN 'Yes' THEN 1 END Filesort, 
+			CASE Q.[Filesort_on_disk] WHEN 'No' THEN 0 WHEN 'Yes' THEN 1 END FilesortOnDisk, 
+			Q.[Merge_passes] MergePasses, 
+			CASE Q.[Priority_queue] WHEN 'No' THEN 0 WHEN 'Yes' THEN 1 END PriorityQueue,
+			@concatLineText QueryStatement
+		FROM @values P
+		PIVOT (
+			MAX(Value)
+			FOR ValueKey IN ([Time], [User@Host], [Thread_id], [Schema], [QC_hit], [Query_time], [Lock_time], [Rows_sent], [Rows_examined], [Rows_affected], [Full_scan], [Full_join], [Tmp_table], [Tmp_table_on_disk], [Filesort], [Filesort_on_disk], [Merge_passes], [Priority_queue])
+			) Q
+
+		----------------------------------------------------------------------------------------
+
+		DELETE FROM @values;
+
+		SET @n = @m;
+
+	END;
+
+	RETURN @entryNumber;
+
+END
--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-# Code-2019.06-SlowLogETL
+# ETL 
+
+ETL process to read in a MariaDb ‘slow query log’.
+
+## Introduction
+
+We use a MariaDb at work, which is a split from MySql.  
+
+Like everything to do with MariaDb / MySql there’s lots of badly documented half baked solutions which you cobble together so you can achieve something and trying
+to ‘optimise’ is one of those things.  I’ve been given this task, but no one really know what ‘optimise’ means or how we might measure it.
+
+MariaDb writes an entry into a ‘slow query log’ if a query takes more than a certain threshold.  The file size for
+our log for a database that is not really that busy is 7GB.  The network admin uses mysterious script to summarise the raw data but I decide to load the ‘slow query log’ itself to perform some analysis that way.
+
+
+## Visual Studio Solution
+
+I created a Visual Studio Solution with two projects:
+
+![Solution](./images/Solution.PNG)
+
+1. A database project to hold the staging table and results.
+2. A SSIS project to move the data into the database project.
+
+### Database Project
+
+This is not about Database Projects per se, but I can really recommend considering them.  You can put your databases into source control and all those other good things that come with that.
+In this project, I’ve created two tables and a stored procedure.
+
+![ProjectDatabase](./images/ProjectDatabase.PNG)
+
+#### Text File Table
+
+The first table is to read in the text file.  I need to the automatically generated line numbers to make sure I keep the order when reading the lines out of the table.
+
+![TableTextFile](./images/TableTextFile.PNG)
+
+#### Slow Queries Table
+
+The second table stores the process rows from the Text File table.  I’ve created a **checksum** as a persisted calculated column
+so I can group similar queries.  I’m taking the checksum of the **SELECT** and **FROM** clauses.  This has the affect
+of removing **USE** statements and everything from the **WHERE** onwards.  If the **SELECT** and the **FROM** (include the **JOIN**s) are the same, I’m assuming it’s the same query.  There’s room for improvement here, but it’s a start.
+
+![TableMariaDbSlowLog](./images/TableMariaDbSlowLog.PNG)
+
+### SSIS
+
+The SSIS project has a single package to read in the text file.
+
+![ProjectSSIS](./images/ProjectSSIS.PNG)
+
+#### Control Flow
+
+The control flow clears the staging table, loads the text file into the staging table, and the stored procedure transforms it and loads it into the result table. 
+
+![SSISControlFlow](./images/SSISControlFlow.PNG)
+
+#### Data Flow
+
+No magic here... reading a text file into a table.
+
+![SSISDataFlow](./images/SSISDataFlow.PNG)
+
+#### Stored Procedure
+
+I’ve included the source code to the stored procedure in this repository.
+
+## Results
+
+This is what it looks like in action.
+
+![SSISSuccess](./images/SSISSuccess.PNG)
+
+### Text File Table
+
+You can see the text file has been loaded with row numbers with the following structure:
+
+1. Header lines starting with #
+2. *n* lines for the query, not starting with a #
+
+The stored procedure loops through the lines and does this:
+
+1. Read in the header lines (the ones starting with a #) and concatenate them (to make the text-split easier)
+2. Build the sql statement by looping until I find a line with a #, i.e. the next block
+3. Insert values into the table
+
+This the log file after it’s read into the FileText table.
+
+![RowsTextFile](./images/RowsTextFile.PNG)
+
+And this is what the final result looks like after the stored procedure has run:
+
+![RowsTableMariaDbSlowLog](./images/RowsTableMariaDbSlowLog.PNG)
+
+Now we can query the table to find out which queries took a long time, or which ones executed particularly often, etc.
+
+Beats a text file!
